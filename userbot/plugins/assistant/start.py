7206f74590480ffb2a4a5d1f35bdd6748fe65800
StarkGang <StarkGangZ@gmail.com>
2020-10-07 12:13:04 +0530

    Update start.py

--- userbot/plugins/assistant/start.py
+++ userbot/plugins/assistant/start.py
@@ -80,3 +80,12 @@ async def all_messages_catcher(event):
         pass
     else:
         await event.forward_to(bot.uid)
+
+
+# Add User To Database ,Later For Broadcast Purpose
+# (C) @SpecHide
+add_user_to_db(
+        fwded_mesg.message_id,
+        message.from_user.id,
+        message.message_id
+    )
