8e258c23b474cf58acd5131d63358de4848c7318
StarkGang <StarkGangZ@gmail.com>
2020-10-09 08:17:43 +0530

    Update botusers_sql.py

--- userbot/plugins/sql_helper/botusers_sql.py
+++ userbot/plugins/sql_helper/botusers_sql.py
@@ -13,7 +13,7 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Stole From Spechide's "No PM's Bot" 😢🤭🚶
+
 """ users Table """
 
 from sqlalchemy import (
@@ -30,12 +30,12 @@ from . import (
 class Users(BASE):
     """ Table to store the received messages """
     __tablename__ = "users"
-    msg_id = Column(Integer, primary_key=True)
+    message_id = Column(Integer, primary_key=True)
     chat_id = Column(String(14))
     um_id = Column(Integer)
 
-    def __init__(self, msg_id, chat_id, um_id):
-        self.msg_id = msg_id
+    def __init__(self, message_id, chat_id, um_id):
+        self.message_id = message_id
         self.chat_id = str(chat_id)  # ensure string
         self.um_id = um_id
 
@@ -46,17 +46,17 @@ class Users(BASE):
 Users.__table__.create(checkfirst=True)
 
 
-def add_user_to_db(msg_id: int, chat_id: int, um_id: int):
+def add_user_to_db(message_id: int, chat_id: int, um_id: int):
     """ add the message to the table """
-    __user = Users(msg_id, str(chat_id), um_id)
+    __user = Users(message_id, str(chat_id), um_id)
     SESSION.add(__user)
     SESSION.commit()
 
 
-def get_user_id(msg_id: int):
+def get_user_id(message_id: int):
     """ get the user_id from the message_id """
     try:
-        s__ = SESSION.query(Users).get(str(msg_id))
+        s__ = SESSION.query(Users).get(str(message_id))
         return int(s__.chat_id), s__.um_id
     finally:
         SESSION.close()
