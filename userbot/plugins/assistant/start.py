5532de34b1b5b9f0f8aa59e83b2180e94a2edad9
StarkGang <StarkGangZ@gmail.com>
2020-10-09 21:11:47 +0530

    Update start.py

--- userbot/plugins/assistant/start.py
+++ userbot/plugins/assistant/start.py
@@ -102,7 +102,7 @@ async def users(event):
 # Bot Permit.
 @tgbot.on(events.NewMessage(func=lambda e: e.is_private))
 async def all_messages_catcher(event):
-    if event.raw_text.startswith("/"):
+    if event.startswith("/"):
         pass
     elif event.from_id == bot.uid:
         pass
